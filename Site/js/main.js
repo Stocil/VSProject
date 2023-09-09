ee2fc46e660b0492e40b13558f6708f596b165c2
Stocil <vashurov02@mail.ru>
2023-09-09 17:48:37 +0300

    First full complited site

--- Site/js/main.js
+++ Site/js/main.js
@@ -1,3 +1,35 @@
-$(function(){
+const body = document.body;
+const burgerButton = body.querySelector(".header__menu-btn");
+const burgerNav = body.querySelector(".header_nav-list");
+const burgerLines = body.querySelectorAll(".header__menu-line");
 
-});
+burgerButton.addEventListener("click", burgerMenuActive);
+
+function burgerMenuActive(event) {
+  body.classList.toggle("scroll-lock");
+  burgerNav.classList.toggle("active");
+
+  burgerLines[0].classList.toggle("menu__line-1");
+  burgerLines[1].classList.toggle("menu__line-2");
+  burgerLines[2].classList.toggle("menu__line-3");
+
+  body.addEventListener("click", clickOutOfNavigation);
+  body.addEventListener("keydown", exitKey);
+}
+
+function clickOutOfNavigation(event) {
+  if (!burgerNav.classList.contains("active")) return;
+  if (event.target.closest(".header_nav-list")) return;
+  if (event.target.closest(".header__menu-btn") === burgerButton) return;
+
+  body.classList.toggle("scroll-lock");
+  burgerNav.classList.toggle("active");
+}
+
+function exitKey(event) {
+  if (!burgerNav.classList.contains("active")) return;
+  if (event.code !== "Escape") return;
+
+  body.classList.toggle("scroll-lock");
+  burgerNav.classList.toggle("active");
+}

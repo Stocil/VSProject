2c4d617ff35b9be2599f3c27e127df450d5a3de1
Stocil <vashurov02@mail.ru>
2023-09-12 17:13:11 +0300

    Vlasov visual take 4 backup

--- Vlasov visual/js/main.js
+++ Vlasov visual/js/main.js
@@ -1,12 +1,18 @@
 const body = document.body;
+
+// Top slider
 const photoSliderInner = body.querySelector(".photo-slider__inner");
 const photoSliderList = body.querySelector(".photo-slider__list");
 
 const photoSliderGap = 12;
 const photoSliderImageWidth = 251;
 
-const photoSliderLeftButton = body.querySelector(".left-btn");
-const photoSliderRightButton = body.querySelector(".right-btn");
+const photoSliderLeftButton = body.querySelector(
+  ".photo-slider__button.left-btn"
+);
+const photoSliderRightButton = body.querySelector(
+  ".photo-slider__button.right-btn"
+);
 
 const photoSliderListWidth = Math.round(
   photoSliderList.childElementCount * photoSliderImageWidth +
@@ -18,7 +24,7 @@ photoSliderList.style.width = photoSliderListWidth + "px";
 
 let topTranslate = 0;
 let isTopScroll = true;
-let topInterval = setInterval(topSliderMove, 100);
+let topInterval = setInterval(topSliderMove, 300);
 
 photoSliderInner.addEventListener("pointerout", topSliderScroll);
 photoSliderInner.addEventListener("pointerover", topSliderStop);
@@ -34,7 +40,7 @@ function topSliderScroll(event) {
 
   isTopScroll = true;
 
-  topInterval = setInterval(topSliderMove, 100);
+  topInterval = setInterval(topSliderMove, 300);
 }
 
 // Move slider, if slider is not over
@@ -102,28 +108,155 @@ function topSliderScrollRight(event) {
   topSliderMove();
 }
 
-// Hover
+// Bottom slider
 
+const gifSliderInner = body.querySelector(".gif-slider__inner");
+const gifSliderList = body.querySelector(".gif-slider__list");
+
+const gifSliderGap = 12;
+const gifSliderImageWidth = 251;
+
+const gifSliderLeftButton = body.querySelector(".gif-slider__button.left-btn");
+const gifSliderRightButton = body.querySelector(
+  ".gif-slider__button.right-btn"
+);
+
+const gifSliderListWidth = Math.round(
+  gifSliderList.childElementCount * gifSliderImageWidth +
+    (gifSliderList.childElementCount - 1) * gifSliderGap -
+    gifSliderList.getBoundingClientRect().left
+);
+
+gifSliderList.style.width = gifSliderListWidth + "px";
+
+let botTranslate = 0;
+let isBotScroll = true;
+let botInterval = setInterval(botSliderMove, 300);
+
+gifSliderInner.addEventListener("pointerout", botSliderScroll);
+gifSliderInner.addEventListener("pointerover", botSliderStop);
+
+window.addEventListener("resize", botCheckSliderRightSide);
+
+gifSliderLeftButton.addEventListener("click", botSliderScrollLeft);
+gifSliderRightButton.addEventListener("click", botSliderScrollRight);
+
+// Сontinue scrolling after the slider stops, if slider is not over
+function botSliderScroll(event) {
+  if (botSliderIsOver()) return;
+
+  isBotScroll = true;
+
+  botInterval = setInterval(botSliderMove, 300);
+}
+
+// Move slider, if slider is not over
+function botSliderMove() {
+  if (botSliderIsOver()) botSliderStop();
+
+  botTranslate -= 10;
+  gifSliderList.style.transform = `translateX(${botTranslate}px)`;
+}
+
+// Stop slider scroll
+function botSliderStop(event) {
+  isBotScroll = false;
+
+  clearInterval(botInterval);
+}
+
+function botSliderIsOver() {
+  if (-botTranslate + gifSliderInner.offsetWidth >= gifSliderListWidth) {
+    gifSliderRightButton.style.opacity = "0";
+    return true;
+  } else {
+    gifSliderRightButton.style.opacity = "1";
+    return false;
+  }
+}
+
+function botCheckSliderRightSide(event) {
+  botSliderMove();
+  if (
+    gifSliderInner.offsetWidth - gifSliderList.getBoundingClientRect().right >
+    gifSliderList.getBoundingClientRect().right - gifSliderInner.offsetWidth
+  ) {
+    botTranslate -=
+      gifSliderList.getBoundingClientRect().right - gifSliderInner.offsetWidth;
+  }
+}
+
+function botSliderScrollLeft(event) {
+  if (botTranslate + 252 > 0) {
+    botTranslate = 0;
+  } else {
+    botTranslate += 252;
+  }
+
+  botSliderMove();
+}
+
+// Limitation of slider buttons
+function botSliderScrollRight(event) {
+  if (-botTranslate + gifSliderInner.offsetWidth + 252 > gifSliderListWidth) {
+    botTranslate = -gifSliderListWidth + gifSliderInner.offsetWidth;
+  } else {
+    botTranslate -= 252;
+  }
+
+  botSliderMove();
+}
+
+// Hover
 photoSliderInner.addEventListener("pointerover", showHeart);
 photoSliderInner.addEventListener("pointerout", hideHeart);
 photoSliderInner.addEventListener("click", likeHeart);
 
+gifSliderInner.addEventListener("pointerover", showHeart);
+gifSliderInner.addEventListener("pointerout", hideHeart);
+gifSliderInner.addEventListener("click", likeHeart);
+
 function showHeart(event) {
-  if (!event.target.classList.contains("photo-slider__item")) return;
+  if (!event.target.classList.contains("slider__item")) return;
 
   event.target.firstElementChild.style.filter = "opacity(50%)";
 }
 
 function hideHeart(event) {
-  if (!event.target.classList.contains("photo-slider__item")) return;
+  if (!event.target.classList.contains("slider__item")) return;
 
   event.target.firstElementChild.style.filter = "";
 }
 
 function likeHeart(event) {
-  if (!event.target.classList.contains("photo-slider__item")) return;
+  if (!event.target.classList.contains("slider__item")) return;
 
   event.target.classList.toggle("like");
+}
+
+// Full screen
+const worksInner = body.querySelector(".works__inner");
+const fullScreenBox = body.querySelector(".full-screen");
+const fullScreenImage = body.querySelector(".full-screen__image");
+const fullScreenCloseButton = body.querySelector(".full-screen__close-button");
+
+worksInner.addEventListener("click", fullScreen);
+fullScreenCloseButton.addEventListener("click", closeFullScreen);
+
+function fullScreen(event) {
+  if (!event.target.classList.contains("slider__images")) return;
+
+  fullScreenImage.src = event.target.src;
+  closeFullScreen();
+}
+
+function closeFullScreen(elem) {
+  setTimeout(() => fullScreenBox.classList.toggle("full-screen__hide"), 300);
+  setTimeout(
+    () => fullScreenImage.classList.toggle("full-screen__image__hide"),
+    300
+  );
+  fullScreenCloseButton.classList.toggle("full-screen__close-button__hide");
 }
 
 // Final slider setting

594b5d38ed1d841c5df6b8937a5cd723121e7a91
Stocil <vashurov02@mail.ru>
2023-09-13 17:49:55 +0300

    Vlasov visual full complite

--- Vlasov visual/js/main.js
+++ Vlasov visual/js/main.js
@@ -454,5 +454,55 @@ function scrollBackgroundSliderRight() {
   }
 }
 
-// Final slider setting
-// -10 300ms
+// Burger
+
+const burgerButton = body.querySelector(".header__menu-btn");
+const burgerNav = body.querySelector(".header__navigation");
+const burgerLines = body.querySelectorAll(".header__menu-line");
+const burgerList = body.querySelector(".header__list");
+
+const sliderLeftButtons = body.querySelectorAll(".left-btn");
+
+const closeClick = new CustomEvent("click");
+
+burgerButton.addEventListener("click", burgerMenuActive);
+
+function burgerMenuActive(event) {
+  body.classList.toggle("scroll-lock");
+  burgerNav.classList.toggle("active");
+  burgerList.classList.toggle("menu__list");
+  burgerButton.classList.toggle("menu__active");
+
+  sliderLeftButtons[0].classList.toggle("arrow__hide");
+  sliderLeftButtons[1].classList.toggle("arrow__hide");
+
+  burgerLines[0].classList.toggle("menu__line-1");
+  burgerLines[1].classList.toggle("menu__line-2");
+  burgerLines[2].classList.toggle("menu__line-3");
+
+  body.addEventListener("click", clickOutOfNavigation);
+  body.addEventListener("keydown", exitKey);
+  burgerList.addEventListener("click", scrollToAnchor);
+}
+
+function clickOutOfNavigation(event) {
+  if (!burgerNav.classList.contains("active")) return;
+  if (event.target.closest(".header_nav-list")) return;
+  if (event.target.closest(".header__menu-btn") === burgerButton) return;
+  if (body.offsetWidth <= 600) return;
+
+  burgerButton.dispatchEvent(closeClick);
+}
+
+function exitKey(event) {
+  if (!burgerNav.classList.contains("active")) return;
+  if (event.code !== "Escape") return;
+
+  burgerButton.dispatchEvent(closeClick);
+}
+
+function scrollToAnchor(event) {
+  if (!event.target.classList.contains("header__list-item")) return;
+
+  burgerButton.dispatchEvent(closeClick);
+}

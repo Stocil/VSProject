19f59e6a685aafd1a04cc029c890be60a87da4fc
Stocil <vashurov02@mail.ru>
2023-09-24 01:20:17 +0300

    Coffe slider add

--- Сoffee house/js/main.js
+++ Сoffee house/js/main.js
@@ -1,58 +1,73 @@
-const body = document.body;
+// const body = document.body;
 
-// Stock slider
+// // Stock slider
 
-// Games slider
+// // Games slider
 
-const gamesSliderInner = body.querySelector(".games__slider__inner");
-const gamesSlider = body.querySelector(".games__slider__list");
-const gamesSliderGap = +getComputedStyle(gamesSlider).gap.slice(0, -2);
-const gamesSliderItemCount = gamesSlider.querySelectorAll(
-  ".games__slider__item"
-).length;
-const gamesSliderGapCount = gamesSliderItemCount - 1;
-const gamesSliderItemWidth = body.querySelector(
-  ".games__slider__item"
-).clientWidth;
+// const gamesSliderInner = body.querySelector(".games__slider__inner");
+// const gamesSlider = body.querySelector(".games__slider__list");
+// const gamesSliderGap = +getComputedStyle(gamesSlider).gap.slice(0, -2);
+// const gamesSliderItemCount = gamesSlider.querySelectorAll(
+//   ".games__slider__item"
+// ).length;
+// const gamesSliderGapCount = gamesSliderItemCount - 1;
+// const gamesSliderItemWidth = body.querySelector(
+//   ".games__slider__item"
+// ).clientWidth;
 
-const gamesSliderWidth =
-  gamesSliderItemCount * gamesSliderItemWidth +
-  gamesSliderGap * gamesSliderGapCount;
+// const gamesSliderWidth =
+//   gamesSliderItemCount * gamesSliderItemWidth +
+//   gamesSliderGap * gamesSliderGapCount;
 
-gamesSlider.style.width = gamesSliderWidth + "px";
+// gamesSlider.style.width = gamesSliderWidth + "px";
 
-// Games slider buttons
+// // Games slider buttons
 
-let gamesSliderTranslate = 0;
+// let gamesSliderTranslate = 0;
 
-gamesSlider.addEventListener("click", gamesSlide);
+// gamesSlider.addEventListener("click", gamesSlide);
 
-function gamesSlide(event) {
-  if (event.target.closest(".games__slider__item").classList.contains("active"))
-    return;
+// function gamesSlide(event) {
+//   if (event.target.closest(".games__slider__item").classList.contains("active"))
+//     return;
 
-  event.preventDefault();
-  const side = whichButton(event.target.closest(".games__slider__item"));
+//   event.preventDefault();
+//   const side = whichButton(event.target.closest(".games__slider__item"));
 
-  if (side === "left") {
-    gamesSliderTranslate += 430;
-  } else if (side === "right") {
-    gamesSliderTranslate -= 430;
-  }
+//   if (side === "left") {
+//     gamesSliderTranslate += 430;
+//   } else if (side === "right") {
+//     gamesSliderTranslate -= 430;
+//   }
 
-  gamesSlider.style.transform = `translateX(${gamesSliderTranslate}px)`;
-  gamesSlider.querySelector(".active").classList.toggle("active");
-  event.target.closest(".games__slider__item").classList.toggle("active");
-}
+//   gamesSlider.style.transform = `translateX(${gamesSliderTranslate}px)`;
+//   // gamesSlider.querySelector(".active").classList.toggle("active");
+//   // event.target.closest(".games__slider__item").classList.toggle("active");
+// }
 
-function whichButton(elem) {
-  // Left
-  if (elem.nextElementSibling) {
-    if (elem.nextElementSibling.classList.contains("active")) return "left";
-  }
+// function whichButton(elem) {
+//   // Left
+//   if (elem.nextElementSibling) {
+//     if (elem.nextElementSibling.classList.contains("active")) return "left";
+//   }
 
-  if (elem.previousElementSibling) {
-    if (elem.previousElementSibling.classList.contains("active"))
-      return "right";
-  }
-}
+//   if (elem.previousElementSibling) {
+//     if (elem.previousElementSibling.classList.contains("active"))
+//       return "right";
+//   }
+// }
+
+new Swiper(".swiper.slider__inner", {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 30,
+
+  keyboard: {
+    enabled: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});

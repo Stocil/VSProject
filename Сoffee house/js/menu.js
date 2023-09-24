f534a2c59c1b8ba4262a6642ca06ddf3b9a10000
Stocil <vashurov02@mail.ru>
2023-09-24 19:58:47 +0300

    coffee menu add

---
+++ Сoffee house/js/menu.js
@@ -0,0 +1,31 @@
+const body = document.body;
+
+const menuSelect = body.querySelector(".menu__type__list");
+const menuBox = body.querySelector(".menu__type__box");
+
+const coffeeShelf = body.querySelector(".menu__shelf__coffee");
+const teaShelf = body.querySelector(".menu__shelf__tea");
+
+menuSelect.addEventListener("click", changeMenuType);
+
+function changeMenuType(event) {
+  if (!event.target.classList.contains("menu__type__image")) return;
+
+  let translate = 0;
+  if (event.target.id === "coffe") {
+    teaShelf.classList.add("hide");
+    coffeeShelf.classList.remove("hide");
+
+    menuBox.style.width = "75px";
+    translate = 0;
+  }
+  if (event.target.id === "dessert") {
+    teaShelf.classList.remove("hide");
+    coffeeShelf.classList.add("hide");
+
+    translate = 102;
+    menuBox.style.width = "82px";
+  }
+
+  menuBox.style.transform = `translateX(${translate}px)`;
+}

ff2a622d69d4aa9894c7d7f8196fd2990a088ded
Stocil <vashurov02@mail.ru>
2023-10-01 19:43:13 +0300

    Coffee full edit added

--- Сoffee house/js/admin.js
+++ Сoffee house/js/admin.js
@@ -2,7 +2,7 @@ const body = document.body;
 
 const asideList = body.querySelector(".admin-panel__sidebar__list");
 const tables = body.querySelector(".admin-panel__tables");
-const editBox = body.querySelector(".admin-panel__edit-box");
+// const editBox = body.querySelector(".admin-panel__edit-box");
 
 asideList.addEventListener("click", makeActive);
 tables.addEventListener("click", removeRow);
@@ -42,54 +42,270 @@ function removeRow(event) {
 
 // Edit button
 
-const editForm = body.querySelector(".admin-panel__form");
-
-const inputId = body.querySelector("textarea#admin-id");
-const inputHeading = body.querySelector("textarea#admin-heading");
-const inputArticle = body.querySelector("textarea#admin-article");
-const inputDate = body.querySelector("input#admin-date");
-
-const formSaveButton = body.querySelector(".admin-panel__form__save");
-const formCancelButton = body.querySelector(".admin-panel__form__cancel");
-
 function editRow(event) {
   if (!event.target.closest(".admin-panel__add-button")) return;
   if (!event.target.classList.contains("admin-panel__button-line")) return;
 
-  event.target.closest(".admin-panel__table").classList.toggle("show");
-  editBox.classList.toggle("show");
+  // Stocks
+  if (event.target.closest(".admin-panel__stock-table")) {
+    event.target.closest(".admin-panel__stock-table").classList.toggle("show");
 
-  // Edit
-  const currentRow = event.target.closest(".admin-panel__table__row");
-  const currentColumns = currentRow.querySelectorAll(
-    ".admin-panel__table__item"
-  );
+    const inputId = body.querySelector("textarea#admin-stock-id");
+    const inputHeading = body.querySelector("textarea#admin-stock-heading");
+    const inputArticle = body.querySelector("textarea#admin-stock-article");
+    const inputDate = body.querySelector("input#admin-stock-date");
 
-  const currentId = currentColumns[0];
-  const currentHeading = currentColumns[1];
-  const currentArticle = currentColumns[2];
-  const currentDate = currentColumns[3];
+    const form = body.querySelector(".admin-panel__stocks-edit-box");
+    const formSaveButton = form.querySelector(".admin-panel__form__save");
+    const formCancelButton = form.querySelector(".admin-panel__form__cancel");
 
-  inputId.value = currentId.innerText;
-  inputHeading.value = currentHeading.innerText;
-  inputArticle.value = currentArticle.innerText;
-  inputDate.value = currentDate.innerText;
+    const stocksEditBox = body.querySelector(".admin-panel__stocks-edit-box");
+    stocksEditBox.classList.toggle("show");
 
-  formSaveButton.addEventListener("click", saveChanges);
-  formCancelButton.addEventListener("click", cancelChanges);
+    // Edit
+    const currentRow = event.target.closest(".admin-panel__table__row");
+    const currentColumns = currentRow.querySelectorAll(
+      ".admin-panel__table__item"
+    );
 
-  function saveChanges() {
-    currentId.innerText = inputId.value;
-    currentHeading.innerText = inputHeading.value;
-    currentArticle.innerText = inputArticle.value;
-    currentDate.innerText = inputDate.value;
+    const currentId = currentColumns[0];
+    const currentHeading = currentColumns[1];
+    const currentArticle = currentColumns[2];
+    const currentDate = currentColumns[3];
 
-    event.target.closest(".admin-panel__table").classList.add("show");
-    editBox.classList.remove("show");
+    inputId.value = currentId.innerText;
+    inputHeading.value = currentHeading.innerText;
+    inputArticle.value = currentArticle.innerText;
+    inputDate.value = currentDate.innerText;
+
+    formSaveButton.addEventListener("click", saveChanges);
+    formCancelButton.addEventListener("click", cancelChanges);
+
+    function saveChanges() {
+      currentId.innerText = inputId.value;
+      currentHeading.innerText = inputHeading.value;
+      currentArticle.innerText = inputArticle.value;
+      currentDate.innerText = inputDate.value;
+
+      event.target.closest(".admin-panel__table").classList.add("show");
+      stocksEditBox.classList.remove("show");
+    }
+
+    function cancelChanges() {
+      event.target.closest(".admin-panel__table").classList.add("show");
+      stocksEditBox.classList.remove("show");
+    }
   }
 
-  function cancelChanges() {
-    event.target.closest(".admin-panel__table").classList.add("show");
-    editBox.classList.remove("show");
+  // Games
+  if (event.target.closest(".admin-panel__game-table")) {
+    event.target.closest("#admin-table-2").classList.toggle("show");
+
+    const inputId = body.querySelector("textarea#admin-games-id");
+    const inputName = body.querySelector("textarea#admin-games-name");
+    const inputDescription = body.querySelector(
+      "textarea#admin-games-description"
+    );
+    const inputDesc = body.querySelector("textarea#admin-games-desc");
+    const inputAbout = body.querySelector("textarea#admin-games-about");
+    const inputRules = body.querySelector("textarea#admin-games-rules");
+
+    const form = body.querySelector(".admin-panel__games-edit-box");
+    const formSaveButton = form.querySelector(".admin-panel__form__save");
+    const formCancelButton = form.querySelector(".admin-panel__form__cancel");
+
+    const gamesEditBox = body.querySelector(".admin-panel__games-edit-box");
+    gamesEditBox.classList.toggle("show");
+
+    // Edit
+    const currentRow = event.target.closest(".admin-panel__table__row");
+    const currentColumns = currentRow.querySelectorAll(
+      ".admin-panel__table__item"
+    );
+
+    const currentId = currentColumns[0];
+    const currentName = currentColumns[1];
+    const currentDescription = currentColumns[2];
+    const currentDesc = currentColumns[3];
+    const currentAbout = currentColumns[4];
+    const currentRules = currentColumns[5];
+
+    inputId.value = currentId.innerText;
+    inputName.value = currentName.innerText;
+    inputDescription.value = currentDescription.innerText;
+    inputDesc.value = currentDesc.innerText;
+    inputAbout.value = currentAbout.innerText;
+    inputRules.value = currentRules.innerText;
+
+    formSaveButton.addEventListener("click", saveChanges);
+    formCancelButton.addEventListener("click", cancelChanges);
+
+    function saveChanges() {
+      currentId.innerText = inputId.value;
+      currentName.innerText = inputName.value;
+      currentDescription.innerText = inputDescription.value;
+      currentDesc.innerText = inputDesc.value;
+      currentAbout.innerText = inputAbout.value;
+      currentRules.innerText = inputRules.value;
+
+      event.target.closest(".admin-panel__table").classList.add("show");
+      gamesEditBox.classList.remove("show");
+    }
+
+    function cancelChanges() {
+      event.target.closest(".admin-panel__table").classList.add("show");
+      gamesEditBox.classList.remove("show");
+    }
+  }
+
+  // Menu
+  if (event.target.closest(".admin-panel__menu-table")) {
+    event.target.closest("#admin-table-3").classList.toggle("show");
+
+    const inputId = body.querySelector("textarea#admin-menu-id");
+    const inputType = body.querySelector("textarea#admin-menu-type");
+    const inputName = body.querySelector("textarea#admin-menu-name");
+    const inputPrice = body.querySelector("textarea#admin-menu-price");
+
+    const form = body.querySelector(".admin-panel__menu-edit-box");
+    const formSaveButton = form.querySelector(".admin-panel__form__save");
+    const formCancelButton = form.querySelector(".admin-panel__form__cancel");
+
+    const menuEditBox = body.querySelector(".admin-panel__menu-edit-box");
+    menuEditBox.classList.toggle("show");
+
+    // Edit
+    const currentRow = event.target.closest(".admin-panel__table__row");
+    const currentColumns = currentRow.querySelectorAll(
+      ".admin-panel__table__item"
+    );
+
+    const currentId = currentColumns[0];
+    const currentType = currentColumns[1];
+    const currentName = currentColumns[2];
+    const currentPrice = currentColumns[3];
+
+    inputId.value = currentId.innerText;
+    inputType.value = currentType.innerText;
+    inputName.value = currentName.innerText;
+    inputPrice.value = currentPrice.innerText;
+
+    formSaveButton.addEventListener("click", saveChanges);
+    formCancelButton.addEventListener("click", cancelChanges);
+
+    function saveChanges() {
+      currentId.innerText = inputId.value;
+      currentType.innerText = inputType.value;
+      currentName.innerText = inputName.value;
+      currentPrice.innerText = inputPrice.value;
+
+      event.target.closest(".admin-panel__table").classList.add("show");
+      menuEditBox.classList.remove("show");
+    }
+
+    function cancelChanges() {
+      event.target.closest(".admin-panel__table").classList.add("show");
+      menuEditBox.classList.remove("show");
+    }
+  }
+
+  // Card
+  if (event.target.closest(".admin-panel__card-table")) {
+    event.target.closest("#admin-table-4").classList.toggle("show");
+
+    const inputId = body.querySelector("textarea#admin-card-id");
+    const inputGameId = body.querySelector("textarea#admin-card-gameId");
+
+    const form = body.querySelector(".admin-panel__card-edit-box");
+    const formSaveButton = form.querySelector(".admin-panel__form__save");
+    const formCancelButton = form.querySelector(".admin-panel__form__cancel");
+
+    const menuEditBox = body.querySelector(".admin-panel__card-edit-box");
+    menuEditBox.classList.toggle("show");
+
+    // Edit
+    const currentRow = event.target.closest(".admin-panel__table__row");
+    const currentColumns = currentRow.querySelectorAll(
+      ".admin-panel__table__item"
+    );
+
+    const currentId = currentColumns[0];
+    const currentGameId = currentColumns[1];
+
+    inputId.value = currentId.innerText;
+    inputGameId.value = currentGameId.innerText;
+
+    formSaveButton.addEventListener("click", saveChanges);
+    formCancelButton.addEventListener("click", cancelChanges);
+
+    function saveChanges() {
+      currentId.innerText = inputId.value;
+      currentGameId.innerText = inputGameId.value;
+
+      event.target.closest(".admin-panel__table").classList.add("show");
+      menuEditBox.classList.remove("show");
+    }
+
+    function cancelChanges() {
+      event.target.closest(".admin-panel__table").classList.add("show");
+      menuEditBox.classList.remove("show");
+    }
+  }
+
+  // Book
+  if (event.target.closest(".admin-panel__book-table")) {
+    event.target.closest("#admin-table-5").classList.toggle("show");
+
+    const inputId = body.querySelector("textarea#admin-book-id");
+    const inputName = body.querySelector("textarea#admin-book-name");
+    const inputNumber = body.querySelector("input#admin-book-number");
+    const inputDate = body.querySelector("input#admin-book-date");
+    const inputTime = body.querySelector("input#admin-book-time");
+
+    const form = body.querySelector(".admin-panel__book-edit-box");
+    const formSaveButton = form.querySelector(".admin-panel__form__save");
+    const formCancelButton = form.querySelector(".admin-panel__form__cancel");
+
+    const menuEditBox = body.querySelector(".admin-panel__book-edit-box");
+    menuEditBox.classList.toggle("show");
+
+    // Edit
+    const currentRow = event.target.closest(".admin-panel__table__row");
+    const currentColumns = currentRow.querySelectorAll(
+      ".admin-panel__table__item"
+    );
+
+    const currentId = currentColumns[0];
+    const currentName = currentColumns[1];
+    const currentNumber = currentColumns[2];
+    const currentDate = currentColumns[3];
+    const currentTime = currentColumns[4];
+
+    inputId.value = currentId.innerText;
+    inputName.value = currentName.innerText;
+    inputNumber.value = currentNumber.innerText;
+    inputDate.value = currentDate.innerText;
+    inputTime.value = currentTime.innerText;
+
+    formSaveButton.addEventListener("click", saveChanges);
+    formCancelButton.addEventListener("click", cancelChanges);
+
+    function saveChanges() {
+      currentId.innerText = inputId.value;
+      currentName.innerText = inputName.value;
+      currentNumber.innerText = inputNumber.value;
+      currentDate.innerText = inputDate.value;
+      currentTime.innerText = inputTime.value;
+
+      event.target.closest(".admin-panel__table").classList.add("show");
+      menuEditBox.classList.remove("show");
+    }
+
+    function cancelChanges() {
+      event.target.closest(".admin-panel__table").classList.add("show");
+      menuEditBox.classList.remove("show");
+    }
   }
 }
+
+// Create new row
